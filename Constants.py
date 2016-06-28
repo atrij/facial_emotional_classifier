e707f08d69390c611dd24389dc0be58fdb4eb0c4
Harsh Pokharna <harsh.81291@gmail.com>
2016-06-29 02:46:54 +0530

    Added a constants Class

---
+++ Constants.py
@@ -0,0 +1,7 @@
+class Constants:
+
+    emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness",
+                "surprise"]
+
+    datasetPathEmotions = "source_emotions"
+    datasetPathImages = "source_images"
--- Main.py
+++ Main.py
@@ -1,14 +1,15 @@
 #Imports
 from BagOfVisualWords import BagOfVisualWords
+from Constants import Constants
 from DatasetOrganiser import DatasetOrganiser
 from ImageProcessor import ImageProcessor
 from ImageProvider import ImageProvider
 from PrincipalComponentAnalysis import PrincipalComponentAnalysis
 from SIFT import SIFT
 
-emotions = ["neutral", "anger", "contempt", "disgust", "fear", "happy", "sadness", "surprise"] #Define emotion order
-datasetPathEmotions = "source_emotions"
-datasetPathImages = "source_images"
+emotions = Constants.emotions
+datasetPathEmotions = Constants.datasetPathEmotions
+datasetPathImages = Constants.datasetPathImages
 
 # Organise dataset
 DatasetOrganiser.organiseDataset(emotions, datasetPathEmotions, datasetPathImages)
@@ -21,16 +22,16 @@ for emotion in emotions:
 trainingData, testData = ImageProvider.splitDataset(emotions)
 
 # Eg. of a training data
-print trainingData["anger"][0]
+print trainingData[emotions[1]][0]
 
 # Eg. of a test data
-print testData["anger"][0]
+print testData[emotions[1]][0]
 
 #Create imageDictionary for training Data
 imageDictionary = ImageProvider.getImageDictionaryFromFilePaths(trainingData, emotions)
 
 # Eg. of an image
-print imageDictionary["anger"][0].shape # 3 Dimensional vector
+print imageDictionary[emotions[1]][0].shape # 3 Dimensional vector
 
 # Create a SIFT object
 sift = SIFT(0, 3, 0.03, 10, 1.6)
@@ -39,19 +40,19 @@ sift = SIFT(0, 3, 0.03, 10, 1.6)
 keyPointsDictionary = sift.detectKeyPointsFromImageDictionary(imageDictionary, emotions)
 
 # Eg. of an image's keypoints
-print len(keyPointsDictionary["anger"][0])
+print len(keyPointsDictionary[emotions[1]][0])
 
 # Get descriptors
 descriptorsDictionary = sift.computeDescriptors(imageDictionary, keyPointsDictionary, emotions)
 
 # Eg. of an image's descriptors
-print descriptorsDictionary["anger"][0].shape # n*128 matrix where n is number of keypoints. Each row is the descriptor for one keypoint.
+print descriptorsDictionary[emotions[1]][0].shape # n*128 matrix where n is number of keypoints. Each row is the descriptor for one keypoint.
 
 # Applying PCA - Principal Component Analysis - It is used for dimensionality reduction
 eigenvectorsDictionary = PrincipalComponentAnalysis.computeEigenvectors(descriptorsDictionary, emotions)
 
 # Eg. of an image's descriptors
-print eigenvectorsDictionary["anger"][0].shape # 128*128 matrix.
+print eigenvectorsDictionary[emotions[1]][0].shape # 128*128 matrix.
 
 #The next step is to use Bag of visual words approach.
 # Create Training Data for clustering
